42dcfbe840865bd95b23afdb2458108c0699daa6
Yobany Caltempa <gioclt26@gmail.com>
2022-03-08 19:49:06 -0500

    commit tree a99d7459be3944d675242de02a1a858b4594db7e

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Write your solution in this file!
+var customerName = "bob";
+
+
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase();
+        return customerName;
+}
+
+
+function setBestCustomer() {
+    bestCustomer = "not bob";
+}
+
+function overwriteBestCustomer() {
+    return (bestCustomer = "maybe bob");
+}
+
+const leastFavoriteCustomer = "me"
+
+function changeLeastFavoriteCustomer() {
+    return (leastFavoriteCustomer = "you");
+}
